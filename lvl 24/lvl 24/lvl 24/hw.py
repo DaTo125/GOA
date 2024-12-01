6626835e09c51d98178a98dcf87d74030863b1d4
DaTo125 <mikeladzedato73@gmail.com>
2024-12-01 11:50:12 +0300

    Add files via upload

---
+++ lvl 24/lvl 24/lvl 24/hw.py
@@ -0,0 +1,8 @@
+#                                      0      1
+message="manqana {0} km/h {1}".format(100, "midis")
+print(message)
+
+arr=["datvi" "mgeli" "lomfxvi" "zviangi..."]
+joined_arr="". join(arr)
+print(joined_arr)
+
